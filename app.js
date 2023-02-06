4b765f1e35b06c4c91d09f247646a27cf957e3af
DeevDevs <dmitriy.vnuchkov@gmail.com>
2023-02-06 17:32:46 +0400

    fix: try origin and credentials

--- app.js
+++ app.js
@@ -12,8 +12,13 @@ const cors = require("cors");
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors());
-app.options("*", cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+// app.options("*", cors());
 // app.use((req, res, next) => {
 //   res.header({ "Access-Control-Allow-Origin": "http://localhost:3000" });
 //   res.header({ "Access-Control-Allow-Credentials": "true" });

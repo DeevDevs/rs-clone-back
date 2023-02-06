4b53d34a35cf163beed6bb6edafb97d70954c18d
DeevDevs <dmitriy.vnuchkov@gmail.com>
2023-02-06 18:44:04 +0400

    fix: cookies samesite and secure

--- helperFns/newCookieOpt.js
+++ helperFns/newCookieOpt.js
@@ -2,6 +2,7 @@ exports.createCookieOptions = (days) => {
   return {
     expires: new Date(Date.now() + days * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    secure: false,
+    sameSite: "None",
+    secure: true,
   };
 };

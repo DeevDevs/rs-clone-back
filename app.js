0c4ee4ed208869ef0bd9379b337764d775db6699
DeevDevs <dmitriy.vnuchkov@gmail.com>
2023-02-06 17:10:45 +0400

    feat: add true to ACAC

--- app.js
+++ app.js
@@ -15,8 +15,9 @@ app.use(cors());
 app.options("*", cors());
 app.use((req, res, next) => {
   res.header({ "Access-Control-Allow-Origin": "http://localhost:3000" });
+  res.header({ "Access-Control-Allow-Credentials": "true" });
   next();
-}); 
+});
 app.use(express.json({ limit: "10kb" }));
 app.use(mongoSanitize());
 app.use(express.static(path.join(__dirname, "public")));

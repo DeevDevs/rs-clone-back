1467100e70fa3d02b87f0e3a7a31d9cb059c235e
DeevDevs <dmitriy.vnuchkov@gmail.com>
2023-02-07 16:29:58 +0400

    fix: add ACAH

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(
 app.use((req, res, next) => {
   res.header({
     "Access-Control-Allow-Origin": "http://localhost:3000",
+    "Access-Control-Allow-Headers": "Authorization",
   });
   next();
 });

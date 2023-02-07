1ff1afc508214d9fb9a489781ff85fb16a7036f1
DeevDevs <dmitriy.vnuchkov@gmail.com>
2023-02-07 14:25:00 +0400

    fix: change request type on heroku

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,7 +8,7 @@ router.post("/signup", authorizeController.signUp);
 router.post("/login", authorizeController.login);
 router.post("/logout", authorizeController.logout);
 router.post("/isloggedin", authorizeController.isLoggedIn);
-router.get("/oneUser", userController.getOneUser);
+router.post("/oneUser", userController.getOneUser);
 
 
 router.use(authorizeController.protect);

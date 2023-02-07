3dbff901971882c92d3bda91b1ffe2d7433bb65c
DeevDevs <dmitriy.vnuchkov@gmail.com>
2023-02-07 17:10:37 +0400

    fix: addOPTIONS handler 4

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ app.use(express.static(path.join(__dirname, "public")));
 // app.use(cors());
 app.use((req, res, next) => {
   res.header({
-    "Access-Control-Allow-Origin": "http://localhost:3000",
+    "Access-Control-Allow-Origin":
+      "http://localhost:3000, https://rs-clone-back.herokuapp.com/api/user/signup",
     "Access-Control-Allow-Headers":
       "Content-Type, Authorization, accept, access-control-allow-origin",
   });

f97c03dc5dd4c18b1280a8bded70a9e433be484a
DeevDevs <dmitriy.vnuchkov@gmail.com>
2023-02-07 17:05:30 +0400

    fix: addOPTIONS handler 2

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers":
       "Content-Type, Authorization, accept, access-control-allow-origin",
   });
+  console.log(req.method);
   if ("OPTIONS" == req.method) res.send(200);
   else next();
 });

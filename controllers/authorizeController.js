093738a47338f232c184ca9f951a445a6f9c5719
DeevDevs <dmitriy.vnuchkov@gmail.com>
2023-02-07 14:02:43 +0400

    fix: remove error from protect 3

--- controllers/authorizeController.js
+++ controllers/authorizeController.js
@@ -125,9 +125,7 @@ exports.isLoggedIn = async (req, res, next) => {
 exports.protect = async (req, res, next) => {
   try {
     let token;
-    console.log(req);
     console.log(req.cookies.jwt);
-    console.log(req.cookies);
     if (
       req.headers.authorization &&
       req.headers.authorization.startsWith("Bearer")

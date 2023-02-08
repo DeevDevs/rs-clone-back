5d6d14e3a45fc5ca0dd0582159347dbea72ce668
DeevDevs <dmitriy.vnuchkov@gmail.com>
2023-02-08 10:05:50 +0400

    fix: block one user delete another user profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,6 +7,7 @@ const jwt = require("jsonwebtoken");
 
 exports.deleteOneUser = async (req, res, next) => {
   try {
+    if (req.body.id === req.user.id) return next(new MyError("You cannot delete another user profile", 401));
     const thisUser = await User.findById(req.body.id);
     if (!thisUser) return next(new MyError("No user found with that ID", 404));
 

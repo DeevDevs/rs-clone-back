40f5c529fa9ad66d785fc5079409f39c776b0ca0
DeevDevs <dmitriy.vnuchkov@gmail.com>
2023-02-09 17:52:48 +0400

    fix: fix reviews fn

--- controllers/memoirController.js
+++ controllers/memoirController.js
@@ -154,12 +154,8 @@ exports.updateOneMemoir = async (req, res, next) => {
 
 exports.getPreviewData = async (req, res, next) => {
   try {
-    console.log(req.user._id);
-    const thisUserID = req.user._id;
-    console.log(thisUserID);
-    if (!thisUserID)
-      return next(new MyError("Please, fix the request URL", 400));
-
+    if (!req.user) return next(new MyError("Please, fix the request URL", 400));
+    
     const memoirIDs = req.user.memoirIDs;
     if (memoirIDs.length === 0) {
       res.status(200).json({
@@ -177,8 +173,8 @@ exports.getPreviewData = async (req, res, next) => {
             memoirName: memoir.tripName,
           };
           resolve(previewData);
+          return;
         }
-        reject(new MyError("Could not find a memoir for preview", 404));
       });
     });
 

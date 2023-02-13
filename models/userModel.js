7254483904f17f649b94999807717a2a3833a0a6
DeevDevs <dmitriy.vnuchkov@gmail.com>
2023-02-13 17:59:26 +0400

    fix: fix user Model

--- models/userModel.js
+++ models/userModel.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     default: "Please, tell us about yourself a little.",
   },
-  bio: {
+  from: {
     type: String,
     default: "",
   },
